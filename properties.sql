a3b5346e143598cae90f0262e2896f9bf3f9aa82
Ramiro Ochoa <ramiro.ochoa@bg.com.bo>
2023-11-28 10:47:35 -0400

    Ok

---
+++ properties.sql
@@ -0,0 +1,10 @@
+INSERT INTO "properties" ("id", "application", "profile", "label", "prop_key", "prop_value") VALUES (7, 'account-manager', 'dev', 'master', 'spring.datasource.url', 'jdbc:postgresql://localhost:5432/accounts');
+INSERT INTO "properties" ("id", "application", "profile", "label", "prop_key", "prop_value") VALUES (10, 'account-manager', 'dev', 'master', 'spring.datasource.username', 'postgres');
+INSERT INTO "properties" ("id", "application", "profile", "label", "prop_key", "prop_value") VALUES (11, 'account-manager', 'dev', 'master', 'spring.datasource.password', '{cipher}422c5ae833e8d6d65ecb92cd2ebaaba8f9a3b285a73eff21078220576345f660');
+INSERT INTO "properties" ("id", "application", "profile", "label", "prop_key", "prop_value") VALUES (12, 'clients-manager', 'dev', 'master', 'spring.datasource.url', 'jdbc:postgresql://localhost:5432/clientes');
+INSERT INTO "properties" ("id", "application", "profile", "label", "prop_key", "prop_value") VALUES (13, 'clients-manager', 'dev', 'master', 'spring.datasource.username', 'postgres');
+INSERT INTO "properties" ("id", "application", "profile", "label", "prop_key", "prop_value") VALUES (14, 'clients-manager', 'dev', 'master', 'spring.datasource.password', '{cipher}422c5ae833e8d6d65ecb92cd2ebaaba8f9a3b285a73eff21078220576345f660');
+INSERT INTO "properties" ("id", "application", "profile", "label", "prop_key", "prop_value") VALUES (15, 'clients-manager', 'dev', 'master', 'account.url.ws', 'http://localhost:8020');
+INSERT INTO "properties" ("id", "application", "profile", "label", "prop_key", "prop_value") VALUES (19, 'clients-manager', 'dev', 'master', 'actualizacion.properties', 'primerValor');
+INSERT INTO "properties" ("id", "application", "profile", "label", "prop_key", "prop_value") VALUES (20, 'clients-manager', 'dev', 'master', 'management.endpoints.web.exposure.include', '*');
+INSERT INTO "properties" ("id", "application", "profile", "label", "prop_key", "prop_value") VALUES (21, 'clients-manager', 'dev', 'master', 'spring.security.oauth2.resourceserver.jwt.issuer-uri', 'http://localhost:8023');
